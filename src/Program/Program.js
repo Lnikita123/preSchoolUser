b0d84db09e018b6220daf26dcf7900c050bbbe6c
Lilhorenikita <nikitapalnesto19@gmail.com>
2023-12-24 20:02:28 +0530

    feat: ui changes

--- src/Program/Program.js
+++ src/Program/Program.js
@@ -3,11 +3,13 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 import Enquire from "../Enquire";
 import Play from "./Play";
-import { useActingStore } from "../store/useActingStore";
+// import { useActStore } from "../store/useActStore";
+// import { useActingStore } from "../store/useActingStore";
 
 const Program = () => {
-  const showActing = useActingStore((s) => s.showActing);
-  const filteredData = useActingStore((s) => s.filteredData);
+  // const showActing = useActingStore((s) => s.showActing);
+  // const filteredData = useActingStore((s) => s.filteredData);
+  // const selected = useActStore((s) => s.selected);
   const [slidesData, setSlidesData] = useState({
     id: 1,
     Photo: "",
@@ -27,27 +29,8 @@ const Program = () => {
   }, []);
   return (
     <>
-      <div
-        style={{
-          backgroundImage: !showActing
-            ? `url(/program.svg)`
-            : `url(${filteredData?.Photos})`,
-        }}
-        className="lg:relative lg:flex flex-col bg-cover lg:w-full w-full h-[14rem] lg:h-[500px] lg:mt-[8rem] mt-[8rem]"
-      >
-        <div className="lg:absolute lg:top-1/2 flex-col lg:flex">
-          <p
-            className="lg:ml-[18rem] text-2xl text-white font-normal"
-            style={{ fontFamily: "Englebert, cursive" }}
-          >
-            {slidesData && slidesData?.Published
-              ? slidesData?.Heading
-              : "Our programs"}
-          </p>
-        </div>
-      </div>
       <div>
-        <Play />
+        <Play slidesData={slidesData} />
       </div>
       <div>
         <Enquire />
